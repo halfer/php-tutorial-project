1d945d66f36d11313bb388906df0724d31759384
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-25 22:41:03 +0000

    Centralise some database code

---
+++ lib/common.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Gets the root path of the project
+ * 
+ * @return string
+ */
+function getRootPath()
+{
+	return realpath(__DIR__ . '/..');
+}
+
+/**
+ * Gets the full path for the database file
+ * 
+ * @return string
+ */
+function getDatabasePath()
+{
+	return getRootPath() . '/data/data.sqlite';
+}
+
+/**
+ * Gets the DSN for the SQLite connection
+ * 
+ * @return string
+ */
+function getDsn()
+{
+	return 'sqlite:' . getDatabasePath();
+}
+
+/**
+ * Gets the PDO object for database acccess
+ * 
+ * @return \PDO
+ */
+function getPDO()
+{
+	return new PDO(getDsn());
+}
