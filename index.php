8e7743fc7c3c0e0f995619b54a60c338e454d7a3
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-25 16:31:27 +0000

    Remove repetition and use a loop to generate dummy post data

--- index.php
+++ index.php
@@ -8,18 +8,14 @@
 		<h1>Blog title</h1>
 		<p>This paragraph summarises what the blog is about.</p>
 
-		<h2>Article 1 title</h2>
-		<div>dd Mon YYYY</div>
-		<p>A paragraph summarising article 1.</p>
-		<p>
-			<a href="#">Read more...</a>
-		</p>
+		<?php for ($postId = 1; $postId <= 3; $postId++): ?>
+			<h2>Article <?php echo $postId ?> title</h2>
+			<div>dd Mon YYYY</div>
+			<p>A paragraph summarising article <?php echo $postId ?>.</p>
+			<p>
+				<a href="#">Read more...</a>
+			</p>
+		<?php endfor ?>
 
-		<h2>Article 2 title</h2>
-		<div>dd Mon YYYY</div>
-		<p>A paragraph summarising article 2.</p>
-		<p>
-			<a href="#">Read more...</a>
-		</p>
 	</body>
 </html>
