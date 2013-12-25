8b492805bfaea954a8511994fc7030beb7ab4faa
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-25 16:25:07 +0000

    Add skeleton front page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>A blog application</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	</head>
+	<body>
+		<h1>Blog title</h1>
+		<p>This paragraph summarises what the blog is about.</p>
+
+		<h2>Article 1 title</h2>
+		<div>dd Mon YYYY</div>
+		<p>A paragraph summarising article 1.</p>
+		<p>
+			<a href="#">Read more...</a>
+		</p>
+
+		<h2>Article 2 title</h2>
+		<div>dd Mon YYYY</div>
+		<p>A paragraph summarising article 2.</p>
+		<p>
+			<a href="#">Read more...</a>
+		</p>
+	</body>
+</html>
