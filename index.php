4ed88be3350c994de443c7f3328e02a322f36bed
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-25 21:49:53 +0000

    Convert front page to read from database

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+
+// Work out the path to the database, so SQLite/PDO can connect
+$root = __DIR__;
+$database = $root . '/data/data.sqlite';
+$dsn = 'sqlite:' . $database;
+
+// Connect to the database, run a query, handle errors
+$pdo = new PDO($dsn);
+$stmt = $pdo->query(
+	'SELECT
+		title, created_at, body
+	FROM
+		post
+	ORDER BY
+		created_at DESC'
+);
+if ($stmt === false)
+{
+	throw new Exception('There was a problem running this query');
+}
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -8,14 +31,20 @@
 		<h1>Blog title</h1>
 		<p>This paragraph summarises what the blog is about.</p>
 
-		<?php for ($postId = 1; $postId <= 3; $postId++): ?>
-			<h2>Article <?php echo $postId ?> title</h2>
-			<div>dd Mon YYYY</div>
-			<p>A paragraph summarising article <?php echo $postId ?>.</p>
+		<?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+			<h2>
+				<?php echo htmlspecialchars($row['title'], ENT_HTML5, 'UTF-8') ?>
+			</h2>
+			<div>
+				<?php echo $row['created_at'] ?>
+			</div>
+			<p>
+				<?php echo htmlspecialchars($row['body'], ENT_HTML5, 'UTF-8') ?>
+			</p>
 			<p>
 				<a href="#">Read more...</a>
 			</p>
-		<?php endfor ?>
+		<?php endwhile ?>
 
 	</body>
 </html>
