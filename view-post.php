442e648fba11247a0230cfeefe344aed077691a9
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-25 22:28:06 +0000

    Centralise title section of page in an HTML fragment

--- view-post.php
+++ view-post.php
@@ -50,8 +50,7 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	</head>
 	<body>
-		<h1>Blog title</h1>
-		<p>This paragraph summarises what the blog is about.</p>
+		<?php require 'templates/title.php' ?>
 
 		<h2>
 			<?php echo htmlspecialchars($row['title']) ?>
