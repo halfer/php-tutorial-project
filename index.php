788c586ce44e9519025604146d556c98d00a787f
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-26 09:52:02 +0000

    Add comment counts to front page

--- index.php
+++ index.php
@@ -32,6 +32,8 @@ if ($stmt === false)
 			</h2>
 			<div>
 				<?php echo convertSqlDate($row['created_at']) ?>
+				
+				(<?php echo countCommentsForPost($row['id']) ?> comments)
 			</div>
 			<p>
 				<?php echo htmlEscape($row['body']) ?>
