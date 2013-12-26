851f4f9b7e4fead5ccf270a55fabae7da6f04b25
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-26 20:38:55 +0000

    Switch on sessions in all pages, recognise logged in/out

--- templates/title.php
+++ templates/title.php
@@ -1,5 +1,9 @@
 <div style="float: right;">
-	<a href="login.php">Log in</a>
+	<?php if (isLoggedIn()): ?>
+		<a href="logout.php">Log out</a>
+	<?php else: ?>
+		<a href="login.php">Log in</a>
+	<?php endif ?>
 </div>
 
 <a href="index.php">
