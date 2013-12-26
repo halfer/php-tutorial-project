589cce9d805557a79823f5798d324a99eac34482
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-26 22:49:21 +0000

    Add a PDO param to functions, for consistency

--- lib/common.php
+++ lib/common.php
@@ -94,12 +94,12 @@ function redirectAndExit($script)
 /**
  * Returns the number of comments for the specified post
  * 
+ * @param PDO $pdo
  * @param integer $postId
  * @return integer
  */
-function countCommentsForPost($postId)
+function countCommentsForPost(PDO $pdo, $postId)
 {
-	$pdo = getPDO();
 	$sql = "
 		SELECT
 			COUNT(*) c
@@ -119,11 +119,12 @@ function countCommentsForPost($postId)
 /**
  * Returns all the comments for the specified post
  * 
+ * @param PDO $pdo
  * @param integer $postId
+ * return array
  */
-function getCommentsForPost($postId)
+function getCommentsForPost(PDO $pdo, $postId)
 {
-	$pdo = getPDO();
 	$sql = "
 		SELECT
 			id, name, text, created_at, website
