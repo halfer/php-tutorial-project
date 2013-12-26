fba5fe0ec4008b6c8ee62b0d65b2300c08ad4176
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-26 09:50:37 +0000

    Add comments to fixtures

--- data/init.sql
+++ data/init.sql
@@ -55,3 +55,42 @@ This is split into paragraphs.",
 		date('now', '-13 days')
 	)
 ;
+
+DROP TABLE IF EXISTS comment;
+
+CREATE TABLE comment (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	post_id INTEGER NOT NULL,
+	created_at VARCHAR NOT NULL,
+	name VARCHAR NOT NULL,
+	website VARCHAR,
+	text VARCHAR NOT NULL
+);
+
+INSERT INTO
+	comment
+	(
+		post_id, created_at, name, website, text
+	)
+	VALUES(
+		1,
+		date('now', '-10 days'),
+		'Jimmy',
+		'http://example.com/',
+		"This is Jimmy's contribution"
+	)
+;
+
+INSERT INTO
+	comment
+	(
+		post_id, created_at, name, website, text
+	)
+	VALUES(
+		1,
+		date('now', '-8 days'),
+		'Jonny',
+		'http://anotherexample.com/',
+		"This is a comment from Jonny"
+	)
+;
