1cf0550331be9de5b85aec4968c9250e23f5469c
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-26 22:43:27 +0000

    Various CSS improvements

--- index.php
+++ index.php
@@ -36,24 +36,28 @@ $notFound = isset($_GET['not-found']);
 			</div>
 		<?php endif ?>
 
-		<?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
-			<h2>
-				<?php echo htmlspecialchars($row['title']) ?>
-			</h2>
-			<div>
-				<?php echo convertSqlDate($row['created_at']) ?>
-				
-				(<?php echo countCommentsForPost($row['id']) ?> comments)
-			</div>
-			<p>
-				<?php echo htmlspecialchars($row['body']) ?>
-			</p>
-			<p>
-				<a
-					href="view-post.php?post_id=<?php echo $row['id'] ?>"
-				>Read more...</a>
-			</p>
-		<?php endwhile ?>
+		<div class="post-list">
+			<?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+				<div class="post-synopsis"> 
+					<h2>
+						<?php echo htmlspecialchars($row['title']) ?>
+					</h2>
+					<div class="meta">
+						<?php echo convertSqlDate($row['created_at']) ?>
+
+						(<?php echo countCommentsForPost($row['id']) ?> comments)
+					</div>
+					<p>
+						<?php echo htmlspecialchars($row['body']) ?>
+					</p>
+					<div class="read-more">
+						<a
+							href="view-post.php?post_id=<?php echo $row['id'] ?>"
+						>Read more...</a>
+					</div>
+				</div>
+			<?php endwhile ?>
+		</div>
 
 	</body>
 </html>
