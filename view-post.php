5a02c57be7b99a97ee52a147a095e5a3f8080fb7
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-26 11:25:38 +0000

    Refactor some business logic to separate library file

--- view-post.php
+++ view-post.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'lib/common.php';
+require_once 'lib/view-post.php';
 
 // Get a sanitised post ID
 if (isset($_GET['post_id']))
@@ -14,28 +15,7 @@ else
 
 // Connect to the database, run a query, handle errors
 $pdo = getPDO();
-$stmt = $pdo->prepare(
-	'SELECT
-		title, created_at, body
-	FROM
-		post
-	WHERE
-		id = :id'
-);
-if ($stmt === false)
-{
-	throw new Exception('There was a problem preparing this query');
-}
-$result = $stmt->execute(
-	array('id' => $postId, )
-);
-if ($result === false)
-{
-	throw new Exception('There was a problem running this query');	
-}
-
-// Let's get a row
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$row = getPostRow($pdo, $postId);
 
 // Swap carriage returns for paragraph breaks
 $bodyText = htmlspecialchars($row['body']);
