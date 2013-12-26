c7155e6a41aec7004d59b4cfe086282975127e61
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-26 12:48:59 +0000

    Add basic commenting form, validation and db error handling

--- lib/view-post.php
+++ lib/view-post.php
@@ -34,3 +34,57 @@ function getPostRow(PDO $pdo, $postId)
 
 	return $row;
 }
+
+/**
+ * Writes a comment to a particular post
+ * 
+ * @param PDO $pdo
+ * @param integer $postId
+ * @param array $commentData
+ * @return array
+ */
+function addCommentToPost(PDO $pdo, $postId, array $commentData)
+{
+	$errors = array();
+
+	// Do some validation
+	if (empty($commentData['name']))
+	{
+		$errors['name'] = 'A name is required';
+	}
+	if (empty($commentData['text']))
+	{
+		$errors['text'] = 'A comment is required';
+	}
+
+	// If we are error free, try writing the comment
+	if (!$errors)
+	{
+		$sql = "
+			INSERT INTO
+				comment
+			(name, website, text, post_id)
+			VALUES(:name, :website, :text, :post_id)
+		";
+		$stmt = $pdo->prepare($sql);
+		if ($stmt === false)
+		{
+			throw new Exception('Cannot prepare statement to insert comment');
+		}
+		$result = $stmt->execute(
+			array_merge($commentData, array('post_id' => $postId, ))
+		);
+
+		if ($result === false)
+		{
+			// @todo This renders a database-level message to the user, fix this
+			$errorInfo = $stmt->errorInfo();
+			if ($errorInfo)
+			{
+				$errors[] = $errorInfo[2];
+			}
+		}
+	}
+
+	return $errors;
+}
