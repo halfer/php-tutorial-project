3fc1afc5e4f802adc8964ad09aa94693146523da
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-26 23:27:17 +0000

    Tidy up CSS for comment form

--- templates/comment-form.php
+++ templates/comment-form.php
@@ -1,6 +1,6 @@
 <?php // Report any errors in a bullet-point list ?>
 <?php if ($errors): ?>
-	<div class="error box">
+	<div class="error box comment-margin">
 		<ul>
 			<?php foreach ($errors as $error): ?>
 				<li><?php echo $error ?></li>
@@ -11,8 +11,8 @@
 
 <h3>Add your comment</h3>
 
-<form method="post">
-	<p>
+<form method="post" class="comment-form">
+	<div>
 		<label for="comment-name">
 			Name:
 		</label>
@@ -22,8 +22,8 @@
 			name="comment-name"
 			value="<?php echo htmlspecialchars($commentData['name']) ?>"
 		/>
-	</p>
-	<p>
+	</div>
+	<div>
 		<label for="comment-website">
 			Website:
 		</label>
@@ -33,8 +33,8 @@
 			name="comment-website"
 			value="<?php echo htmlspecialchars($commentData['website']) ?>"
 		/>
-	</p>
-	<p>
+	</div>
+	<div>
 		<label for="comment-text">
 			Comment:
 		</label>
@@ -44,7 +44,9 @@
 			rows="8"
 			cols="70"
 		><?php echo htmlspecialchars($commentData['text']) ?></textarea>
-	</p>
+	</div>
 
-	<input type="submit" value="Submit comment" />
+	<div>
+		<input type="submit" value="Submit comment" />
+	</div>
 </form>
