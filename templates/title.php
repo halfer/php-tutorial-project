955f7bf704a26953ff5457f9bfa93dd8dc79ca83
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-26 20:44:53 +0000

    Add logout and username greeting

--- templates/title.php
+++ templates/title.php
@@ -1,5 +1,6 @@
 <div style="float: right;">
 	<?php if (isLoggedIn()): ?>
+	Hello <?php echo htmlEscape(getAuthUser()) ?>.
 		<a href="logout.php">Log out</a>
 	<?php else: ?>
 		<a href="login.php">Log in</a>
