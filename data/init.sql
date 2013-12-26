a73883878bf93de483e4d74be414229fb556f480
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-26 11:04:18 +0000

    Convert all dates to datetimes

--- data/init.sql
+++ data/init.sql
@@ -24,7 +24,7 @@ INSERT INTO
 
 It is split into paragraphs.",
 		1,
-		date('now', '-2 months')
+		datetime('now', '-2 months', '-45 minutes', '+10 seconds')
 	)
 ;
 
@@ -38,7 +38,7 @@ INSERT INTO
 		"This is the body of the second post.
 This is another paragraph.",
 		1,
-		date('now', '-40 days')
+		datetime('now', '-40 days', '+815 minutes', '+37 seconds')
 	)
 ;
 
@@ -52,7 +52,7 @@ INSERT INTO
 		"This is the body of the third post.
 This is split into paragraphs.",
 		1,
-		date('now', '-13 days')
+		datetime('now', '-13 days', '+198 minutes', '+51 seconds')
 	)
 ;
 
@@ -74,7 +74,7 @@ INSERT INTO
 	)
 	VALUES(
 		1,
-		date('now', '-10 days'),
+		datetime('now', '-10 days', '+231 minutes', '+7 seconds'),
 		'Jimmy',
 		'http://example.com/',
 		"This is Jimmy's contribution"
@@ -88,7 +88,7 @@ INSERT INTO
 	)
 	VALUES(
 		1,
-		date('now', '-8 days'),
+		datetime('now', '-8 days', '+549 minutes', '+32 seconds'),
 		'Jonny',
 		'http://anotherexample.com/',
 		"This is a comment from Jonny"
