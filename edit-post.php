64b25df52dc6fbc7c3430a6e042291779276f794
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-27 14:52:48 +0000

    Add in custom title in edit post

--- edit-post.php
+++ edit-post.php
@@ -78,7 +78,13 @@ if ($_POST)
 		<?php require 'templates/head.php' ?>
 	</head>
 	<body>
-		<?php require 'templates/title.php' ?>
+		<?php require 'templates/top-menu.php' ?>
+
+		<?php if (isset($_GET['post_id'])): ?>
+			<h1>Edit post</h1>
+		<?php else: ?>
+			<h1>New post</h1>
+		<?php endif ?>
 
 		<?php if ($errors): ?>
 			<div class="error box">
