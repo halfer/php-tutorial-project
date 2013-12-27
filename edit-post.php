655184481696da5e30472ee5a4bc8695497711e9
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-27 13:26:26 +0000

    Add basic new post functionality

--- edit-post.php
+++ edit-post.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'lib/common.php';
+require_once 'lib/edit-post.php';
 
 session_start();
 
@@ -7,6 +8,45 @@ session_start();
 if (!isLoggedIn())
 {
 	redirectAndExit('index.php');
+}
+
+// Handle the post operation here
+$errors = array();
+if ($_POST)
+{
+	// Validate these first
+	$title = $_POST['post-title'];
+	if (!$title)
+	{
+		$errors[] = 'The post must have a title';
+	}
+	$body = $_POST['post-body'];
+	if (!$body)
+	{
+		$errors[] = 'The post must have a body';
+	}
+
+	if (!$errors)
+	{
+		$pdo = getPDO();
+		$userId = getAuthUserId($pdo);
+		$postId = addPost(
+			getPDO(),
+			$title,
+			$body,
+			$userId
+		);
+
+		if ($postId === false)
+		{
+			$errors[] = 'Post operation failed';
+		}
+	}
+
+	if (!$errors)
+	{
+		redirectAndExit('edit-post.php?post_id=' . $postId);
+	}
 }
 
 ?>
@@ -18,6 +58,16 @@ if (!isLoggedIn())
 	</head>
 	<body>
 		<?php require 'templates/title.php' ?>
+
+		<?php if ($errors): ?>
+			<div class="error box">
+				<ul>
+					<?php foreach ($errors as $error): ?>
+						<li><?php echo $error ?></li>
+					<?php endforeach ?>
+				</ul>
+			</div>
+		<?php endif ?>
 
 		<form method="post" class="post-form user-form">
 			<div>
