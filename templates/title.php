16110336ef0a1ad8c98cdf85685667079e0d4270
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-27 14:50:30 +0000

    Separate the top menu into a new file

--- templates/title.php
+++ templates/title.php
@@ -1,15 +1,4 @@
-<div class="top-menu">
-	<div class="menu-options">
-		<?php if (isLoggedIn()): ?>
-			<a href="edit-post.php">New post</a>
-			|
-			Hello <?php echo htmlspecialchars(getAuthUser()) ?>.
-			<a href="logout.php">Log out</a>
-		<?php else: ?>
-			<a href="login.php">Log in</a>
-		<?php endif ?>
-	</div>
-</div>
+<?php require 'top-menu.php' ?>
 
 <a href="/">
 	<h1>Blog title</h1>
