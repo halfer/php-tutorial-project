c1ceb33542b33c234305499f00f19adb01084727
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-28 21:03:08 +0000

    Upgrade login form with global CSS features

--- login.php
+++ login.php
@@ -57,19 +57,29 @@ if ($_POST)
 		
 		<form
 			method="post"
+			class="user-form"
 		>
-			<p>
-				Username:
+			<div>
+				<label for="username">
+					Username:
+				</label>
 				<input
 					type="text"
+					id="username"
 					name="username"
 					value="<?php echo htmlEscape($username) ?>"
 				/>
-			</p>
-			<p>
-				Password:
-				<input type="password" name="password" />
-			</p>
+			</div>
+			<div>
+				<label for="password">
+					Password:
+				</label>
+				<input
+					type="password"
+					id="password"
+					name="password"
+				/>
+			</div>
 			<input type="submit" name="submit" value="Login" />
 		</form>
 	</body>
