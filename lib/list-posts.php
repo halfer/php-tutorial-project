8aa16b92d258de30535d22a1f97c48511fe84832
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-28 20:48:48 +0000

    Allow posts that have comments to be deleted

--- lib/list-posts.php
+++ lib/list-posts.php
@@ -3,6 +3,8 @@
 /**
  * Tries to delete the specified post
  * 
+ * We first delete the comments attached to the post, and then delete the post itself
+ * 
  * @param PDO $pdo
  * @param integer $postId
  * @return boolean Returns true on successful deletion
@@ -10,21 +12,37 @@
  */
 function deletePost(PDO $pdo, $postId)
 {
-	$sql = "
-		DELETE FROM
+	$sqls = array(
+		// Delete comments first, to remove the foreign key objection
+		"DELETE FROM
+			comment
+		WHERE
+			post_id = :id",
+		// Now we can delete the post
+		"DELETE FROM
 			post
 		WHERE
-			id = :id
-	";
-	$stmt = $pdo->prepare($sql);
-	if ($stmt === false)
-	{
-		throw new Exception('There was a problem preparing this query');
-	}
-
-	$result = $stmt->execute(
-		array('id' => $postId, )
+			id = :id",
 	);
+
+	foreach ($sqls as $sql)
+	{
+		$stmt = $pdo->prepare($sql);
+		if ($stmt === false)
+		{
+			throw new Exception('There was a problem preparing this query');
+		}
+
+		$result = $stmt->execute(
+			array('id' => $postId, )
+		);
+
+		// Don't continue if something went wrong
+		if ($result === false)
+		{
+			break;
+		}
+	}
 
 	return $result !== false;
 }
