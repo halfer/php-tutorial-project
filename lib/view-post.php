3d11714772f881d6269768cd568ef4d16f7285d8
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-28 20:41:04 +0000

    Merge comment-counting into get post query

--- lib/view-post.php
+++ lib/view-post.php
@@ -97,7 +97,8 @@ function getPostRow(PDO $pdo, $postId)
 {
 	$stmt = $pdo->prepare(
 		'SELECT
-			title, created_at, body
+			title, created_at, body,
+			(SELECT COUNT(*) FROM comment WHERE comment.post_id = post.id) comment_count
 		FROM
 			post
 		WHERE
