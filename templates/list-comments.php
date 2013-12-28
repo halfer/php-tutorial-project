d548fe247480ff022dcee17c5cc31acac630b780
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-28 16:01:59 +0000

    Move comments markup to separate template file

---
+++ templates/list-comments.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @var $pdo PDO
+ * @var $postId integer
+ */
+?>
+<div class="comment-list">
+	<h3><?php echo countCommentsForPost($pdo, $postId) ?> comments</h3>
+
+	<?php foreach (getCommentsForPost($pdo, $postId) as $comment): ?>
+		<div class="comment">
+			<div class="comment-meta">
+				Comment from
+				<?php echo htmlspecialchars($comment['name']) ?>
+				on
+				<?php echo convertSqlDate($comment['created_at']) ?>
+			</div>
+			<div class="comment-body">
+				<?php // This is already escaped ?>
+				<?php echo convertNewlinesToParagraphs($comment['text']) ?>
+			</div>
+		</div>
+	<?php endforeach ?>
+</div>
--- view-post.php
+++ view-post.php
@@ -79,24 +79,7 @@ else
 			<?php echo convertNewlinesToParagraphs($row['body']) ?>
 		</div>
 
-		<div class="comment-list">
-			<h3><?php echo countCommentsForPost($pdo, $postId) ?> comments</h3>
-
-			<?php foreach (getCommentsForPost($pdo, $postId) as $comment): ?>
-				<div class="comment">
-					<div class="comment-meta">
-						Comment from
-						<?php echo htmlspecialchars($comment['name']) ?>
-						on
-						<?php echo convertSqlDate($comment['created_at']) ?>
-					</div>
-					<div class="comment-body">
-						<?php // This is already escaped ?>
-						<?php echo convertNewlinesToParagraphs($comment['text']) ?>
-					</div>
-				</div>
-			<?php endforeach ?>
-		</div>
+		<?php require 'templates/list-comments.php' ?>
 
 		<?php require 'templates/comment-form.php' ?>
 	</body>
