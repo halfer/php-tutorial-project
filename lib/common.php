039e7d9b2eda5d43251cb2f76db33293ee5231d7
Jon Hinks <projects.ws@jondh.me.uk>
2014-08-25 23:56:08 +0100

    Only look up users who are enabled

--- lib/common.php
+++ lib/common.php
@@ -141,6 +141,7 @@ function tryLogin(PDO $pdo, $username, $password)
 			user
 		WHERE
 			username = :username
+			AND is_enabled = 1
 	";
 	$stmt = $pdo->prepare($sql);
 	$stmt->execute(
@@ -192,6 +193,7 @@ function getAuthUserId(PDO $pdo)
 			user
 		WHERE
 			username = :username
+			AND is_enabled = 1
 	";
 	$stmt = $pdo->prepare($sql);
 	$stmt->execute(
