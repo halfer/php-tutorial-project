20d666be2bd1a5b9a0dd965ed7d2b190159f58bd
Jon Hinks <projects.ws@jondh.me.uk>
2014-10-03 13:55:58 +0100

    Centralise HTML escaping functions

--- view-post.php
+++ view-post.php
@@ -42,7 +42,7 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
 	<head>
 		<title>
 			A blog application |
-			<?php echo htmlspecialchars($row['title'], ENT_HTML5, 'UTF-8') ?>
+			<?php echo htmlEscape($row['title']) ?>
 		</title>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	</head>
@@ -50,13 +50,13 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
 		<?php require 'templates/title.php' ?>
 
 		<h2>
-			<?php echo htmlspecialchars($row['title'], ENT_HTML5, 'UTF-8') ?>
+			<?php echo htmlEscape($row['title']) ?>
 		</h2>
 		<div>
 			<?php echo $row['created_at'] ?>
 		</div>
 		<p>
-			<?php echo htmlspecialchars($row['body'], ENT_HTML5, 'UTF-8') ?>
+			<?php echo htmlEscape($row['body']) ?>
 		</p>
 	</body>
 </html>
